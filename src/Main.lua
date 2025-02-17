6c19a0057ac1154bc9e60b992cd477fc776c286e
VGRamses <82609091+VGRamses@users.noreply.github.com>
2025-02-16 21:51:28 -0800

    Update Main.lua

--- src/Main.lua
+++ src/Main.lua
@@ -29,7 +29,6 @@ loadstring(game:HttpGet("https://raw.githubusercontent.com/Exunys/Roblox-Functio
 
 local GUI = loadstring(game:HttpGet("https://raw.githubusercontent.com/Exunys/AirHub-V2/main/src/UI%20Library.lua"))()
 local ESP = loadstring(game:HttpGet("https://raw.githubusercontent.com/Exunys/Exunys-ESP/main/src/ESP.lua"))()
-local Aimbot = loadstring(game:HttpGet("https://raw.githubusercontent.com/Exunys/Aimbot-V3/main/src/Aimbot.lua"))()
 
 --// Variables
 
